e73f425b97629ba76855bdba96f47e2f085605b8
shota-tech <iwmtsht@gmail.com>
2023-03-09 12:44:59 +0900

    add base layout

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,10 +1,25 @@
 import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Box, Container, Heading, Flex, Spacer } from '@chakra-ui/react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <Box as='header' h='12' bg='teal'>
+        <Flex direction='row' h='full' px='8' align='center' justify='center'>
+          <Heading color='gray.50' size='md'>
+            APP NAME
+          </Heading>
+          <Spacer />
+          <Heading color='gray.50' size='md'>
+            MENUE
+          </Heading>
+        </Flex>
+      </Box>
+      <Box as='main' bg='gray.50'>
+        <Container maxW='container.xl' minH='100vh'>
+          <Component {...pageProps} />
+        </Container>
+      </Box>
     </ChakraProvider>
   )
 }
